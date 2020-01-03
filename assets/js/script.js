a709967c454c148640afdfe05eeabd29e1c554d6
kqarlos <kqarlos@hotmail.com>
2020-01-03 07:21:27 -0800

    Converts html file to pdf

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 var request = require("request");
 var inquirer = require("inquirer");
 var electron = require("electron");
-var convertFactory = require("electron-html-to");
 var axios = require("axios");
+const fs = require('fs');
+const convertFactory = require('electron-html-to');
 
 var userName = "";
 var color = "";
@@ -56,6 +57,40 @@ inquirer.prompt([
 
 })
 
+
+fs.readFile('index.html', 'utf8', (err, htmlString) => {
+  // add local path in case your HTML has relative paths
+//   htmlString = htmlString.replace(/href="|src="/g, match => {
+//      return match + 'file://path/to/you/base/public/directory';
+//   });
+  const conversion = convertFactory({
+    converterPath: convertFactory.converters.PDF,
+    allowLocalFilesAccess: true
+  });
+  conversion({ html: htmlString }, (err, result) => {
+    if (err) return console.error(err);
+    result.stream.pipe(fs.createWriteStream('anywhere.pdf'));
+    conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+  });
+});
+
+// var fs = require('fs'),
+//     convertFactory = require('electron-html-to');
+
+// var conversion = convertFactory({
+//     converterPath: convertFactory.converters.PDF
+// });
+
+// conversion({ file: 'index.html' }, function (err, result) {
+//     if (err) {
+//         return console.error(err);
+//     }
+
+//     console.log(result.numberOfPages);
+//     console.log(result.logs);
+//     result.stream.pipe(fs.createWriteStream('anywhere.pdf'));
+//     conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+// });
 
 
 
